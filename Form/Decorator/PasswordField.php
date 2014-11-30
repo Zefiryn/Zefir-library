a76db3b21820ec88fc407d10072d5432fb5d0c75
Zefiryn <zefiryn@jewula.net>
2014-11-30 20:40:19 +0100

    Improvements to password field decorator

--- Form/Decorator/PasswordField.php
+++ Form/Decorator/PasswordField.php
@@ -1,29 +1,31 @@
 <?php
 
-class Zefir_Decorator_PasswordField extends Zend_Form_Decorator_Abstract
-{ 
-	/**
+class Zefir_Decorator_PasswordField extends Zend_Form_Decorator_Abstract {
+
+    /**
      * Render an password type element
      * @param  string $content
      * @return string
      */
-    public function render($content)
-    {
+    public function render($content) {
         $element = $this->getElement();
 
-        $value         = $element->getValue($element);
-        $attribs       = $element->getAttribs();
-        $name          = $element->getFullyQualifiedName();
-        $id            = $element->getId();
+        $value = $element->getValue($element);
+        $attribs = $element->getAttribs();
+        $name = $element->getFullyQualifiedName();
+        $id = $element->getId();
         $attribs['id'] = $id;
-        $class = $element->hasErrors() ? $attribs['class'].' '.'error' : $attribs['class'];
-        
-        
-        $elementContent  = 
-        '<input type="password" name="'.$name.'"	id="'.$id.'" value="" 
-        	class="'.$class.'" size="'.$attribs['size'].'" 
-        	maxlength="'.$attribs['maxlength'].'" />';
+        $class = $element->hasErrors() ? $attribs['class'] . ' ' . 'error' : $attribs['class'];
 
-		return $elementContent;
+
+        $elementContent = '<input type="password" name="' . $name . '"	id="' . $id . '" value="" 
+        	class="' . $class . '" size="' . $attribs['size'] . '"';
+        if (array_key_exists('maxlength', $attribs)) {
+            $elementContent .= 'maxlength="' . $attribs['maxlength'] . '"';
+        }
+        $elementContent .= ' />';
+
+        return $elementContent;
     }
+
 }
